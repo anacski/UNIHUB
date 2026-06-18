1febc75155c125766bc9c2008f1a9e93a3d7f89a
Ana Carolina <anacski2@gmail.com>
2026-06-17 23:51:19 -0300

    adicionando lógica interativa e redefinindo localização dos arquivos css e image na página index

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
 const btnEntrar = document.getElementById('btn-entrar');
 const inputEmail = document.getElementById('email');
 const inputSenha = document.getElementById('senha');
+const btnCrieConta = document.getElementById('btn-crie-conta');
 
-btnEntrar.addEventListener('click', function() {
-    const email = inputEmail.value;
-    const senha = inputSenha.value;
+if (btnEntrar) {
+    btnEntrar.addEventListener('click', function() {
+        const email = inputEmail.value;
+        const senha = inputSenha.value;
 
-    if (email === "" || senha === "") {
-        alert("Por favor, preencha todos os campos!");
-    } else if (email.includes("@unirio.br")) {
-        window.location.href = "timeline.html"; 
-    } else {
-        alert("Use um e-mail institucional da UNIRIO.");
-    }
-});
+        if (email === "" || senha === "") {
+            alert("Por favor, preencha todos os campos!");
+        } else if (email.includes("@unirio.br") || email.includes("@edu.unirio.br")) { 
+            window.location.href = "timeline.html"; 
+        } else {
+            alert("Use um e-mail institucional da UNIRIO.");
+        }
+    });
+}
+
+if (btnCrieConta) {
+    btnCrieConta.addEventListener('click', function() {
+        window.location.href = "cadastro1.html";
+    });
+}
